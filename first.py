f9c94cc3c4607a3498b4b1ad63ce7727c7afde30
= <=>
2023-07-08 00:03:14 +0300

    Data Types

--- first.py
+++ first.py
@@ -1,1 +1,30 @@
-"print ("I love python") print ("I love programming")" 
+# first
+# print("I love python")
+# print("I love programming")
+
+# variables
+
+# str
+# first_name = "Wagdy"
+# last_name = "Ahmed"
+# full_name = first_name + " " + last_name
+# print("hello, " + full_name)
+# print(type(first_name))
+
+# int
+# age = 30
+# age += 1
+# print(age)
+# print(type(age))
+# print("your age is:" + str(age))
+
+# float
+# height = 250.5
+# print(type(height))
+# print(height)
+# print("Your height is: " + str(height) + "cm")
+
+# boolean
+human = True
+# print(type(human))
+print("are you a human:" + str(human))
